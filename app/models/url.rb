ace0f45ba0bd26ab908bf943ab3b88eb1705e4bf
Bhaskar <bhaskar8088@gmail.com>
2013-11-25 23:44:47 +0530

    Updated model Url

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,2 +1,39 @@
 class Url < ActiveRecord::Base
+    validates :url, presence: true
+
+    validates :short, uniqueness: true
+    
+    before_save :create_short
+    before_save :create_secret
+
+    private
+
+    def create_short
+        short = self.short
+        if short
+            if short.strip() != ""
+                return
+            end
+        end
+        short = rand(36**4).to_s(36)
+        while Url.find_by(short: short)
+            short = rand(36**4).to_s(36)
+        end
+        self.short = short
+    end
+
+    def create_secret
+        secret = self.secret
+        if secret
+            if secret.strip() != ""
+                return
+            end
+        end
+        secret = rand(36**4).to_s(36)
+        while Url.find_by(secret: secret)
+            secret = rand(36**4).to_s(36)
+        end
+        self.secret = secret
+    end
+
 end
